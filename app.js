5a0ff725e69a290dee620b4299766828bb308476
peitzuchen <prettysna@hotmail.com>
2023-12-22 14:09:46 +0800

    add static page(edit,expense,new)

--- app.js
+++ app.js
@@ -9,8 +9,20 @@ app.set("views", "./views");
 
 app.get("/",(req,res)=> {
 
-  res.render("index")
+  res.redirect("/total")
 })
+
+app.get("/expense", (req, res) => {
+  res.render("expense");
+});
+
+app.get("/expense/new", (req, res) => {
+  res.render("new");
+});
+
+app.get("/expense/id/edit", (req, res) => {
+  res.render("edit");
+});
 
 app.listen(port,()=>{
 console.log(`express server listening on http://localhost:${port}`);
