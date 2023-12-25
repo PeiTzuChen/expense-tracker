21d0e44a9d366fce8b431393e629fc08eaac9cd3
peitzuchen <prettysna@hotmail.com>
2023-12-25 15:12:36 +0800

    implement listening page

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 const { engine } = require("express-handlebars");
-const db = require("./models")
+const db = require("./models");
 const Category = db.Category;
 const Record = db.Record;
 
@@ -10,12 +10,43 @@ app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
 
-app.get("/",(req,res)=> {
-  res.redirect("/total")
-})
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.redirect("/expense");
+});
 
 app.get("/expense", (req, res) => {
-  res.render("expense");
+  Record.findAll({
+    attributes: ["id", "name", "date", "amount", "categoryId"],
+    raw: true,
+    include: [
+      {
+        model: Category,
+        attributes: ["name"],
+      },
+    ],
+  }).then((record) => {
+    res.render("expense",{record});
+  });
+  
+});
+
+app.post("/expense", (req, res) => {
+  const { name, date, category, amount } = req.body;
+
+  Category.findOne({ where: { name: category }, raw: true })
+    .then((category) => {
+      return Record.create({
+        name,
+        date,
+        amount,
+        categoryId: category.id,
+      });
+    })
+    .then(() => {
+      res.redirect("/expense");
+    });
 });
 
 app.get("/expense/new", (req, res) => {
@@ -26,19 +57,6 @@ app.get("/expense/id/edit", (req, res) => {
   res.render("edit");
 });
 
-app.listen(port,()=>{
-console.log(`express server listening on http://localhost:${port}`);
-})
-
-app.use(express.urlencoded({ extended: true }));
-
-app.post("/expense", (req, res) => {
-  const { name, date, category, amount } = req.body;
-
-  Category.findOne({where: {name:category},raw:true})
-  .then((category)=>{
-    return Record.create({name,date,amount,userId,categoryId:category.id})
-  }).then(()=>{
-    res.redirect('/expense')
-  })
+app.listen(port, () => {
+  console.log(`express server listening on http://localhost:${port}`);
 });
