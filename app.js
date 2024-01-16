9d333c2341da0bc0195501905ff2981c40f224c0
peitzuchen <prettysna@hotmail.com>
2024-01-16 18:32:52 +0800

    adjust ebextension

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
 }
 const { engine } = require("express-handlebars");
